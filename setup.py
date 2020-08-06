3f8aaaf60db3149f89864d613d565641b1f316fc
mahulivishal <vishalmahuli8@gmail.com>
2020-08-06 12:40:29 +0530

    changed classifier

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Python Modules',
+    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.7',      #Specify which pyhton versions that you want to support
   ],
